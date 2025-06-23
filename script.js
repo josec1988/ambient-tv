73fadef4daa2e9969a75f140ed9bdb511489ac10
jose1988 <coreas1126@gmail.com>
2025-06-23 15:19:19 -0400

    Added weather icon to weather widget

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// script.js
-
 // OpenWeather API Key
 const apiKey = '242e274db84f68040ac3b0017f9953ce';
 
-// Target city (New York for now)
+// Target city
 const city = 'New York';
 
 // Fetch Weather Data
@@ -11,8 +9,15 @@ async function fetchWeather() {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
   const data = await response.json();
 
+  // Extract weather info
+  const temp = Math.round(data.main.temp);
+  const condition = data.weather[0].main;
+  const iconCode = data.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
   // Update weather widget
-  document.querySelector('#weather-widget p:nth-of-type(1)').textContent = `${Math.round(data.main.temp)}°F - ${data.weather[0].main}`;
+  document.querySelector('#weather-icon').src = iconUrl;
+  document.querySelector('#weather-widget p:nth-of-type(1)').textContent = `${temp}°F - ${condition}`;
   document.querySelector('#weather-widget p:nth-of-type(2)').textContent = `${data.name}`;
 }
 
